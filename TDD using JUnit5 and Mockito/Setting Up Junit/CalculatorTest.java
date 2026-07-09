82144db930016d36ddf5515cf7d56613a3c0a4f4
suhashritha <mallapureddysuhashritha@gmail.com>
2026-07-09 19:21:47 +0530

    Add unit test for Calculator addition method

---
+++ TDD using JUnit5 and Mockito/Setting Up Junit/CalculatorTest.java
@@ -0,0 +1,17 @@
+package org;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(10, 20);
+
+        assertEquals(30, result);
+    }
+}
